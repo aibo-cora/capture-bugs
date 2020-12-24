cd66604e5b4c13a3f4b03efa76851af55d985fb0
Yura Filatov <yura.fila@gmail.com>
2020-12-24 11:10:44 -0500

    Initial Commit

---
+++ capture-bugs/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  capture-bugs
+//
+//  Created by Yura on 12/24/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
